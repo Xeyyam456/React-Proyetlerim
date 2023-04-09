9b2e3ab1efb8bb7c8a297fc2199e61ec7172a9d1
Xeyyam456 <Xeyyamelizade5@gmail.com>
2023-04-09 05:25:51 +0400

    ilk create islemi

---
+++ proje1/src/pages/Login/login.jsx
@@ -0,0 +1,14 @@
+import './loginstyle.css';
+import React from 'react'
+
+const login = () => {
+    return (
+        <div className='login'> 
+
+            <h1>salam</h1>
+
+        </div>
+    )
+}
+
+export default login;
